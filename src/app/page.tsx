931cda71b80be531fba6b033ede4f0976819429d
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-02-05 21:34:53 -0300

    :bug: fix: removendo react.fragment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import About from "./pages/About/About.page";
 import HomePage from "./pages/Home/Home.page";
 import Phrase from "./pages/Phrase/Phrase.page";
@@ -7,12 +6,12 @@ import Contact from "./pages/Contact/Contact.page";
 
 export default function Home() {
   return (
-    <React.Fragment>
+    <>
       <HomePage />
       <Phrase />
       <About />
       <Skills />
       <Contact />
-    </React.Fragment>
+    </>
   );
 }
