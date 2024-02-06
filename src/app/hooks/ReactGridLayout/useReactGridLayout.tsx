5b7f6966ef48c51297d38682fcc4bc7ccc56ed29
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-02-05 21:24:09 -0300

    :sparkles: feat: react grid layout

---
+++ src/app/hooks/ReactGridLayout/useReactGridLayout.tsx
@@ -0,0 +1,38 @@
+"use client";
+import GridLayout from "react-grid-layout"
+"/node_modules/react-grid-layout/css/styles.css"
+"/node_modules/react-resizable/css/styles.css"
+import "./style.css";
+
+export interface ReactGridLayoutProps {
+  col: number
+  layout: GridLayout.Layout[]
+  components?: { key: string, component: JSX.Element }[]
+}
+
+export const ReactGridLayout = (props: ReactGridLayoutProps) => {
+  const { col, layout, components } = props
+
+  const width = window.innerWidth
+
+  return (
+    <GridLayout
+      layout={layout}
+      className="layout"
+      isDraggable={true}
+      isResizable={true}
+      margin={[12, 12]}
+      containerPadding={[0, 0]}
+      rowHeight={30}
+      width={width * 0.95}
+      cols={col}
+      draggableCancel=".preventDrag"
+    >
+      {components?.map((item) => (
+        <div key={item.key} style={{ width: 'auto', height: '100%', display: 'flex' }}>
+          {item.component}
+        </div>
+      ))}
+    </GridLayout>
+  )
+}
